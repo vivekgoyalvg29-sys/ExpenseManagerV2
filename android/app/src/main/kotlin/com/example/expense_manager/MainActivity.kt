0966aa362fa2b32af2f6df60a3c93e648ce73388
vivekgoyalvg29-sys <vivekgoyal.vg29@gmail.com>
2026-03-11 17:07:37 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/expense_manager/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/expense_manager/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.expense_manager
 
+import android.content.Intent
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity() {
+
     override fun getInitialRoute(): String {
         val openRecords = intent?.getBooleanExtra("open_records", false) ?: false
         return if (openRecords) "/transactions" else "/"
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
     }
 }
