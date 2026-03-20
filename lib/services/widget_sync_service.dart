a37ff494afdf07d237dd86a644ed78342f19eabf
vivekgoyalvg29-sys <vivekgoyal.vg29@gmail.com>
2026-03-20 15:50:44 +0530

    Refine Android expense widget layout

--- lib/services/widget_sync_service.dart
+++ lib/services/widget_sync_service.dart
@@ -109,6 +109,10 @@ class WidgetSyncService {
       'currentPeriodLabel',
       DateFormat('MMMM').format(now),
     );
+    await HomeWidget.saveWidgetData<String>(
+      'currentMonthWidgetLabel',
+      DateFormat('MMMM-yy').format(now),
+    );
     await HomeWidget.saveWidgetData<double>('currentMonthBudget', currentMonthBudget);
     await HomeWidget.saveWidgetData<double>('currentMonthExpense', currentMonthExpense);
     await HomeWidget.saveWidgetData<double>('currentMonthPercentage', currentMonthPercentage);
